70226640c090dea047a4e8a0bbd8d60edc8cdbb9
Sriram Sattiraju <sriramsattiraju07@gmail.com>
2025-12-28 22:57:12 -0600

    first data scrape

--- infoScraper.py
+++ infoScraper.py
@@ -17,3 +17,14 @@ soup = BeautifulSoup(html, "lxml")
 
 # soup is represented as a dom tree
 
+div_main = soup.find("div", role="main")
+div_info = div_main.find("div", class_="nine columns")
+
+addr_h5 = div_info.find("h5", string=lambda s: s and "Facility Address" in s) # works well with typecasting
+addr_para = addr_h5.find_next("p")
+
+print(addr_para)
+
+
+
+
