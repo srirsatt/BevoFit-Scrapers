eb844453e76aa34111268bd11411e16e0f9b8976
Sriram Sattiraju <sriramsattiraju07@gmail.com>
2025-12-28 21:55:10 -0600

    updated gitignore, env

--- infoScraper.py
+++ infoScraper.py
@@ -1,2 +1,19 @@
 # one time scraper -> just scrapes the static page of activities&features at a facility
 
+# first pull desired webpage
+
+# easy example
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.utrecsports.org/facilities/facility/bellmont-hall"
+
+html = requests.get(url).text
+
+# this should be replaced with a supabase grab and index pipeline
+
+soup = BeautifulSoup(html, "lxml")
+
+# soup is represented as a dom tree
+
