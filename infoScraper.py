92e2d99e9edb3d9a550b26cfe157d83daaf66e29
Sriram Sattiraju <sriramsattiraju07@gmail.com>
2025-12-28 23:33:12 -0600

    .env addition

--- infoScraper.py
+++ infoScraper.py
@@ -5,7 +5,11 @@
 # easy example
 
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString, Tag
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+load_dotenv()
 
 url = "https://www.utrecsports.org/facilities/facility/bellmont-hall"
 
@@ -23,7 +27,18 @@ div_info = div_main.find("div", class_="nine columns")
 addr_h5 = div_info.find("h5", string=lambda s: s and "Facility Address" in s) # works well with typecasting
 addr_para = addr_h5.find_next("p")
 
-print(addr_para)
+lines = []
+for node in addr_para.contents:
+    if isinstance(node, Tag) and node.name == 'a':
+        break # stops at first a tag
+
+    if isinstance(node, NavigableString):
+        text = node.strip()
+        if text: # otherwise, just appends my text where i need it if the node is simply text (skips br, for example)
+            lines.append(text)
+    
+address = ", ".join(lines).replace("\xa0", " ") # for each new line, join with a comma, replace the weird formatting thingy with a blank
+print(address)
 
 
 
